11a571884ba558c8506c21efb59fa992f86cc6cd
Maximiliano Galoto <69372752+MGaloto@users.noreply.github.com>
2024-02-12 13:23:56 -0300

    Update configdb.py

--- configdb.py
+++ configdb.py
@@ -4,31 +4,13 @@ import pyodbc
 import os
 from dotenv import load_dotenv
 import json
-from datetime import datetime
-import pprint
 
 load_dotenv()
 
 class ConfigDataBase():
-    """
-    En esta clase se hace una instancia en la base de datos local MSSQL luego de restaurar la base de datos.
-    En primero lugar se modifica la tabla quedandonos con el ultimo registro con FECHA_COPIA ultima por cada fila 
-    duplicada en ID, MUESTRA, y RESULTADO.
-    En segundo lugar se modifica la tabla para que solo se pueda hacer APPEND de una combinacion unica de [ID], [MUESTRA], [RESULTADO], [FECHA_COPIA] y no puedan insertarse duplicados.
-    """
+
     def __init__(self, table):
         self.table = table
-        self.logsConnection = []
-        self.logsDataBase = []
-        self.date = self.getDateNow()
-
-    
-    def getDateNow(self):
-        fecha_hora_actual = datetime.now()
-        formato = "%Y-%m-%d %H:%M:%S.%f"
-        date = fecha_hora_actual.strftime(formato)[:-3]
-        return date
-        
 
     def run(self):
         try:
